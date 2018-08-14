985e1809fd8ee5b6a3d3324b1f516b4ad31d5c73
Haggerty <dhaggerty@pronet.com>
2018-08-14 11:06:31 -0500

    added a Spell class that spellchecks richTextBoxes 9individually)... Need to switch out my text column for richtextboxes and apply the Spell class to it

--- photolog/DataGridViewRichTextBoxColumn.cs
+++ photolog/DataGridViewRichTextBoxColumn.cs
@@ -6,7 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.ComponentModel;
 
-namespace DataGridViewRichTextBox
+namespace photolog
 {
     public class DataGridViewRichTextBoxColumn : DataGridViewColumn
     {
